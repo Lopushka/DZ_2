af208347318c95ef12077d794dec2c0b3d1a3b70
Lopushka <sv13yat@gmail.com>
2022-12-02 23:36:55 +0300

    2,5 version ex_1_d\z

--- Ex_1/Program.cs
+++ Ex_1/Program.cs
@@ -1,35 +1,37 @@
-﻿// новый вариант задания (самое простое решение)
+﻿// Первый вариант задания/
 
-Console.Write("\nГенерирую случайное число");
-Random rnd = new Random();
-int roll = rnd.Next(100, 1000);
-Console.WriteLine($"\nСлучайное число: {roll}");
-roll /= 10;
-var too = roll % 10;
-Console.WriteLine($"\nВторая цифра случайного числа: {too}");
+Console.Write("Введите число от 100 до 999: ");
+int numder = Convert.ToInt32(Console.ReadLine());
+if (numder < 100)
+{
+    Console.Write("\nПрочти еще раз первое сообщение!!");
 
-// Первый вариант задания был интересней.
+}
+if (numder > 999)
+{
+    Console.Write("\nПрочти еще раз первое сообщение!!");
 
-// Console.Write("Введите число от 100 до 999: ");
-// int numder = Convert.ToInt32(Console.ReadLine());
-// if (numder < 100)
-// {
-//     Console.Write("\nПрочти еще раз первое сообщение!!");
+}
 
-// }
-// if (numder > 999)
-// {
-//     Console.Write("\nПрочти еще раз первое сообщение!!");
+while (numder >= 100 && numder <= 999)
+{
+    numder /= 10;
+    var too = numder % 10;
+    Console.WriteLine($"\nвторая цифра введенного числа: {too}");
+    break;
+}
 
-// }
+// // новый вариант задания (самое простое решение)
 
-// while (numder >= 100 && numder <= 999)
-// {
-//     numder /= 10;
-//     var too = numder % 10;
-//     Console.WriteLine($"\nвторая цифра введенного числа: {too}");
-//     break;
-// }
+// Console.Write("\nГенерирую случайное число");
+// Random rnd = new Random();
+// int roll = rnd.Next(100, 1000);
+// Console.WriteLine($"\nСлучайное число: {roll}");
+// roll /= 10;
+// var too = roll % 10;
+// Console.WriteLine($"\nВторая цифра случайного числа: {too}");
+
+
 //  через массив
 // char[] myArray = roll.ToString().ToCharArray();
 // Console.WriteLine($"\nВторая цифра вашего числа это: {myArray[1]}");
