b493ed1c7b02d6e52c2d856d262e1e5d63040c33
Lopushka <sv13yat@gmail.com>
2022-12-03 16:07:07 +0300

    .\EX_2\

---
+++ EX_2/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Give my a namber: ");
+int numder = Convert.ToInt32(Console.ReadLine());
+if (numder <= 99)
+{
+    Console.Write("\n3 digits not included");
+}
+else
+{
+    if (numder >= 100 && numder < 1000)
+    {
+        var thee = numder % 10;
+        Console.Write($"\nthird digit of the number: {thee} ");
+    }
+
+    if (numder >= 1000)
+    {
+        string fake_number = numder.ToString();  // кривой способ
+        Console.Write($"\nthird digit of the number: {fake_number[2]}");
+    }
+
+    // array
+    // if( numder >=100)  
+    // { 
+    //  char[] myArray = numder.ToString().ToCharArray();
+    //  Console.WriteLine($"\nthird digit of the number: {myArray[2]}");
+    // }
+
+}
---
+++ EX_3/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("\nВведите чисдо которое проверить на кратность: ");
+int numder = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\n1 кратное число: ");
+int numder_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("\n2 кратное число: ");
+int numder_2 = Convert.ToInt32(Console.ReadLine());
+
+if (numder % numder_1 == 0 && numder % numder_2 == 0)
+{
+    Console.Write($"\nЧисло \"{numder}\" кратно числу \"{numder_1}\" и кратно числу \"{numder_2}\"");
+}
+
+else
+{
+    if (numder % numder_1 != 0 && numder % numder_2 == 0)
+    {
+        Console.Write($"\nЧисло \"{numder}\" не кратно числу \"{numder_1}\" и кратно числу \"{numder_2}\"");
+    }
+
+    if (numder % numder_1 == 0 && numder % numder_2 != 0)
+    {
+        Console.Write($"\nЧисло \"{numder}\" кратно числу \"{numder_1}\" и не кратно числу \"{numder_2}\"");
+    }
+
+    if (numder % numder_1 != 0 && numder % numder_2 != 0)
+    {
+        Console.Write($"\nЧисло \"{numder}\" не кратно числу \"{numder_1}\" и не кратно числу \"{numder_2}\"");
+    }
+}
